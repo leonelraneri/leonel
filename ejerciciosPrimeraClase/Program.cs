00da4079e94a1e2a145bd81b8b26aef871769654
unknown <leonelraneri@hotmail.com>
2021-10-04 11:53:34 -0300

    'primer_commit'

---
+++ ejerciciosPrimeraClase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejerciciosPrimeraClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
